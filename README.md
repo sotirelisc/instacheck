905c823360c40e60ebd2cfbdda054172109eb27a
sotirelisc <gtakriz@hotmail.com>
2014-11-03 00:36:35 +0200

    Improvements.

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Check if an Instagram profile is public or private using the username.
 USAGE:
 Call isPrivate passing the target username and your CLIENT ID (provided when registering an app as developer on Instagram).
 
+Returns:
+true 	- if the user is private
+false 	- if the user is public
+unknown - if the state cannot be retreived
+-1 	- if the user is not found
+
 The script does NOT require an ACCESS TOKEN (granting target user's permission)!
--- instacheck.php
+++ instacheck.php
@@ -8,17 +8,20 @@ ini_set('html_errors', 1);
 // Get the profile ID through username
 function getID($username, $client_id) {
     $username = strtolower($username); // Instagram usernames are always in lowercase
-    $get = file_get_contents("https://api.instagram.com/v1/users/search?q={$username}&client_id={$client_id}");
-    $json = json_decode($get);
-
-    // Iterate through json result and search for our username
-    foreach ($json->data as $user) {
-        if ($user->username == $username) {
-            return $user->id;
+    // Make sure that the request sent is formed correctly
+    if (getResponse("https://api.instagram.com/v1/users/search?q={$username}&client_id={$client_id}") != "400") {
+        $get = file_get_contents("https://api.instagram.com/v1/users/search?q={$username}&client_id={$client_id}");
+        $json = json_decode($get);
+        // Iterate through json result and search for our username
+        foreach ($json->data as $user) {
+            if ($user->username == $username) {
+                return $user->id;
+            }
         }
+        return false; // User was not found
+    } else {
+	return -1; // Bad request
     }
-
-    return 0; // User was not found
 }
 
 // Get the HTTP response code
@@ -30,23 +33,28 @@ function getResponse($url) {
 // Check profile state
 function isPrivate($username, $client_id) {
     $user_id = getID($username, $client_id);
-    echo "User {$username} (ID: {$user_id}) is ";
-    if ($user_id > 0) {
+    if ($user_id != false && $user_id != -1) {
 	// Response code 400 (bad request) means that we can't get any info (private profile)
 	if (getResponse("https://api.instagram.com/v1/users/{$user_id}/media/recent/?client_id={$client_id}") != "400") {
 	    $get = file_get_contents("https://api.instagram.com/v1/users/{$user_id}/media/recent/?client_id={$client_id}");	
 	    $json = json_decode($get);
 	    // Code 200 means that the profile is public
 	    if ($json->meta->code == 200) {
-	        return 'public';
+		$pic;
+		// Iterate through data to get profile picture
+		foreach ($json->data as $res) {
+		    $pic = $res->user->profile_picture;
+		}
+		echo "<br /><img src='{$pic}' /><br />";
+	        return false;
 	    } else { // Code other than 200 or 400
 	        return 'unknown';
 	    }
 	} else {
-	    return 'private';
+	    return true;
 	}
     } else {
-	return 'id not found';    
+	return -1;
     }
 }
 
